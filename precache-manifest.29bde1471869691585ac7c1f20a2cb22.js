3f4ec6043233c81c2c158d99bd760a0fe96db01c
BILLY ENDRES <billyendres@BILLYs-MacBook-Pro.local>
2019-05-22 15:47:03 +1000

    Updates

---
+++ precache-manifest.29bde1471869691585ac7c1f20a2cb22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ee66cb6096f5e8bfe519ea9a65d23d",
+    "url": "/React-Mini_Youtube/index.html"
+  },
+  {
+    "revision": "08ba9b8e6dd92130493f",
+    "url": "/React-Mini_Youtube/static/css/main.4629e43d.chunk.css"
+  },
+  {
+    "revision": "fa74e842c7dc2c0d80ad",
+    "url": "/React-Mini_Youtube/static/js/2.b5f6079a.chunk.js"
+  },
+  {
+    "revision": "08ba9b8e6dd92130493f",
+    "url": "/React-Mini_Youtube/static/js/main.2d5faa27.chunk.js"
+  },
+  {
+    "revision": "b0fa861d88e9aa4f71f2",
+    "url": "/React-Mini_Youtube/static/js/runtime~main.f0dd1e44.js"
+  }
+]);
